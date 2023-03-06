937b98e8aa98d3486ed765995722c2198c596547
BrandonDeoram <brandondeoram8931@gmail.com>
2023-03-06 14:39:09 -0500

    128.

--- 128. LongestConsecSequence.py
+++ 128. LongestConsecSequence.py
@@ -1,7 +1,16 @@
-from collections import Counter
 def longestConsecutive(nums):
-    hashMap = Counter(nums)
-    return hashMap
+    maxLength=0
+    length=0
+    hashSet=set(nums)
+    for num in hashSet:
+        # Starting of a Sequence
+        if((num-1) not in hashSet):
+            while((num)+length in hashSet):
+                print('in')
+                length+=1
+                maxLength = max(length,maxLength)
+            length=0
+    return maxLength
 
-nums = [100,4,200,1,3,2]
+nums = [0,3,7,2,5,8,4,6,0,1]
 print(longestConsecutive(nums))
